cb7e2ddcf1868e044b37d6b5b4f705f6e2b01257
devang pawar <80813053+devangpawar17@users.noreply.github.com>
2022-10-03 12:14:31 +0530

    smoothing

---
+++ index.js
@@ -0,0 +1,10 @@
+function run(){
+    console.log("hello");
+
+  //   document.querySelectorAll('[id=model]').forEach(elem=>{
+  //   elem.setAttribute("scale"," 1 1 1")
+  //   console.log(elem.getAttribute("scale"));
+  // })
+
+  }
+
